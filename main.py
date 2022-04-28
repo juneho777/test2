370d22ab13100f044a322b3ec77158c94eb44dcc
june777 <fea7@naver.com>
2022-04-27 20:52:45 -0400

    commit1

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 #changing
+#wow
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
