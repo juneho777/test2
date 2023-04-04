0d516e46102f6825878ed498cc1bea5582081800
june777 <fea7@naver.com>
2023-04-03 21:37:38 -0400

    yes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-#master2
+#m2
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
