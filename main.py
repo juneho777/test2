d7ebd4e10e550bc41a278c553b685571b442901a
juneho hwang <jh882@cornell.edu>
2023-04-03 22:45:22 -0400

    ee

--- main.py
+++ main.py
@@ -8,17 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
 
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#q
-
-
-#aaaaa
-
-
-
-#mmm4
-
-
-#mastering4
+#ee
 
 
 # Press the green button in the gutter to run the script.
