dbea34099ad6362c928966fa8daf34f85c5e0487
juneho777 <jh882@cornell.edu>
2023-04-05 16:01:51 -0400

    Revert "yes3"
    This reverts commit 5689eb29

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ def print_hi(name):
 #ok
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
-    print_hi('PyCharm  no')
-#aaa
+    print_hi('PyCharm2222')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
