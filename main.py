0e37df9f6fd5d4a147290a0ebe1a54f3392e6f39
juneho777 <jh882@cornell.edu>
2023-04-05 16:02:31 -0400

    aa

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def print_hi(name):
 #ok
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharmaa')
+    print_hi('PyCharmaaaa')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
