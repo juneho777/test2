2e4d19e7b7079c2fa3fe3373efa0e4d7337f8c24
juneho hwang <35974764+juneho777@users.noreply.github.com>
2023-04-05 16:06:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    print_hi('PyCharm  hew')
-#aaa
+    print_hi('PyCharm  3333')
+#4444
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
