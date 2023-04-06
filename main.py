6368960b474bcbe878deae5b9fd5655ba4ecec23
Patrick <fea7@naver.com>
2023-04-05 20:38:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,9 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
+    print_hi('PyCharm3')
 
-
-
-    print_hi('PyCharm1')
-
-    #2
-
-
-
+    #4
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
