16ffefd3ff7687c3c29b99dd3527a3e355779656
june777 <fea7@naver.com>
2023-04-06 11:05:43 -0400

    add

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    print_hi('PyCharm33new')
+    print_hi('PyCharm3')
 
-    #44mew
+    #4
 
 
 
